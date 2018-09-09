155d485bba2e191a8e7ebd9b25151b33c7f368fc
nombiezinja <tianyizhang1987@gmail.com>
2018-09-08 19:45:23 -0700

    adding migrations and assocaitions for the rest of the tables

--- db/migrate/20180909022250_create_agreements.rb
+++ db/migrate/20180909022250_create_agreements.rb
@@ -2,7 +2,12 @@ class CreateAgreements < ActiveRecord::Migration[5.2]
   def change
     create_table :agreements do |t|
       t.timestamps
+      t.string :description
+      t.datetime :start_time
+      t.datetime :end_time
     end
+    add_reference :agreements, :created_by, foreign_key: { to_table: :users },null: false
+    add_reference :agreements, :validated_by, foreign_key: { to_table: :users },null: false
     add_foreign_key :agreements, :pairs
   end
 end
--- db/migrate/20180909022300_create_cancellations.rb
+++ db/migrate/20180909022300_create_cancellations.rb
@@ -1,7 +1,6 @@
 class CreateCancellations < ActiveRecord::Migration[5.2]
   def change
     create_table :cancellations do |t|
-
       t.timestamps
     end
   end
